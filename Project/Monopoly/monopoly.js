ec4f07fb12c0e205f1ca3388330a632ae6ac40bf
JoelYoungberg <cgecko@gmail.com>
2014-12-03 10:22:45 -0800

    monopoly update
    board piece now travels all the way around the board...

--- Project/Monopoly/monopoly.js
+++ Project/Monopoly/monopoly.js
@@ -4,8 +4,10 @@
 	var plrAry = [];
 	
 	var globalLoc = 0;
-
-
+	var prevLoc = 0;
+	
+	var propAry = [];
+	
 //Dice Roll Functions/Objects
 	function Dice(){
 		var Dice = {
@@ -29,6 +31,15 @@
 		DiceRoll = Dice();
 		document.getElementById("diceDisplay").innerHTML = "<h4>"+DiceRoll.die1+" "+DiceRoll.die2+" "+"</h4>";
 		
+		prevLoc = globalLoc; //Storing previous location here to delete later
+		
+		globalLoc += DiceRoll.sum(); //Add sum of dice to location to determine where to move
+		
+		if(globalLoc >= 40){ //If player reaches or passes GO, reset location 
+		                     //Note: this would be a good place to add the $200 collect feature
+			globalLoc -= 40;
+		}
+		
 		//First test object...
 	var P1 = {
 		name: "Joel",
@@ -44,16 +55,22 @@
 	//CurPlyr = P1;//Use array index and add to index.../reset when at end to beginning etc. etc. 
 	//Move player according to diceroll result
 	//CurPlyr.location = DiceRoll.die1+DiceRoll.die2;
-	globalLoc += DiceRoll.sum();
+	
 	
 	var View = {
 		displayMove: function(location){
 			var boardSpace = document.getElementById(location);
 			boardSpace.setAttribute("class", "avatar");
+		},
+		deleteOldMove: function(location){
+			var oldBoardSpace = document.getElementById(location);
+			oldBoardSpace.removeAttribute("class");
 		}
 	};
 	
+	View.deleteOldMove(prevLoc);
 	View.displayMove(P1.location);
+	
 		
 		
 	}//End function rollDice////////////////////////////////////////////////////////////

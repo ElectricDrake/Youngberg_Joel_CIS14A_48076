e44c9dcf371455c6678c041dcdfc04c5c728e451
JoelYoungberg <cgecko@gmail.com>
2014-12-07 15:54:19 -0800

    updating business/bonsai site
    added admin login and cookies!

--- Project/Monopoly/monopoly.js
+++ Project/Monopoly/monopoly.js
@@ -54,8 +54,17 @@
 		displayMove: function(location){
 			var token = document.getElementById("p" + plrIndx);
 			document.getElementById(location).appendChild(token);
-		}
-	};
+		},
+		displayMoney: function(){
+			// Putting name and money stats in display area
+			var txt = " ";
+			for(var k = 0; k < numPlyrs; k++){
+			txt += plrAry[k].name+": "+plrAry[k].money+" ";
+			}
+			document.getElementById("moneyDisplay").innerHTML = txt;
+		}//End function display money
+	};//End view
+	View.displayMoney();
 	
 	
 	//Dice Function Declaration
@@ -84,11 +93,8 @@
 		
 		//Player Selection happens here...
 	    //Array Index decides what player is current... 
-	
-	    //plrAry[plrIndx];//Use array index and change index to change players../reset when at end to beginning etc. etc. 
+	    //Use array index and change index to change players../reset when at end to beginning etc. etc. 
 	    //Move between players after each roll unless doubles are rolled....
-	    
-		plrAry[plrIndx].prevLoc = plrAry[plrIndx].location; //Storing previous location here to delete later
 		
 		plrAry[plrIndx].location += DiceRoll.sum(); //Add sum of dice to location to determine where to move
 		
@@ -100,11 +106,52 @@
 	View.displayMove(plrAry[plrIndx].location);
 	
 	//Buy property stuff here
-	buy(plrAry[plrIndx].location);//Do and inner HTML message box instead of an alert
+	//Write a function that takes in the current player's location, then prompts
+	//Based on the location for that particular property...
+	function buy(location, plrIndx2){
+		//First check if location is a purchaseable property
+		if("owned" in boardAry[location]){
+			if(boardAry[location].owned === false){
+			document.getElementById("messageDisplay").innerHTML = "<h5>"+"This property is available"+"</h5>";
+			var btn = document.createElement("BUTTON");
+			var text = document.createTextNode("Buy Property");
+			btn.appendChild(text); //Putting text into button
+			btn.setAttribute("id", "buyBtn");
+			
+			document.getElementById("messageDisplay").appendChild(btn);
+			document.getElementById("buyBtn").onclick = function handleBuy(){
+			var buyPrmt = confirm("Are you sure you want to buy this property?");
+				if (buyPrmt){//if buy prompt is true (or user clicks ok)
+					//Perform Buy actions
+					plrAry[plrIndx2].money -= boardAry[location].price;//Subtracting money from buyer
+					View.displayMoney();//updating display
+					
+					boardAry[location].owned = true;
+					boardAry[location].ownedBy = plrIndx2;
+			  }//End if buy prompt
+			}//End Function handleBuy
+		  }//End if owned = false
+		  
+		  else if(boardAry[location].owned === true){
+			  
+		  document.getElementById("messageDisplay").innerHTML = "<h5>"+boardAry[location].name+" is owned by "+plrAry[boardAry[location].ownedBy].name+"</h5>"; 
+		  	  //Subtract Rent amount of property from 
+			  var rent = boardAry[location].rent;
+			  alert("You must pay "+plrAry[boardAry[location].ownedBy].name+" $"+rent);
+			  plrAry[plrIndx2].money -= rent;
+			  //Get access to property owner's money and pay them! Store owner's plr index in owned by!!! instead of name!!
+			  plrAry[boardAry[location].ownedBy].money += rent;
+			  View.displayMoney();//updating display
+		  }
+		}//End if owned in boardAry location
+		else{// if the property is owned or not for sale....
+			document.getElementById("messageDisplay").innerHTML = "<h5>"+"This board space not for sale"+"</h5>";
+		}	
+	}//End function Buy
+	buy(plrAry[plrIndx].location, plrIndx);//Calling buy property function here
 	
 	plrIndx += 1;//Switch player
 	
-	console.log(DiceRoll.doubles());
 	if(DiceRoll.doubles())
 	plrIndx -= 1;//If doubles are rolled... it is the same player's turn again
 	
--- Project/Monopoly/property.js
+++ Project/Monopoly/property.js
@@ -3,11 +3,12 @@
 var boardAry = [];//Array of board spaces
 
 //Property Constructor
-function Property(name, price, rent, owned){
+function Property(name, price, rent, owned, ownedBy){
 	this.name = name;
 	this.price = price;
 	this.rent = rent; //Change rent to a function that determines what rent is later
 	this.owned = owned;
+	this.ownedBy = ownedBy;
 }
 
 function RailRoad(name, price, rent, owned, numOwned){
@@ -69,55 +70,35 @@ var IncTax = {
 	boardAry[10] = VisitJail;
 	boardAry[11] = new Property("St. Charles Place", 140, 70, false);
 	boardAry[12] = Utility;
-	boardAry[13] = new Property("Mediterranean Avenue", 60, 30, false);
-	boardAry[14] = new Property("Mediterranean Avenue", 60, 30, false);
+	boardAry[13] = new Property("States Avenue", 60, 30, false);
+	boardAry[14] = new Property("Virginia Avenue", 60, 30, false);
 	boardAry[15] = new RailRoad("Pennsylvania Railroad", 200, 60, false, 1);
-	boardAry[16] = new Property("Mediterranean Avenue", 60, 30, false);
+	boardAry[16] = new Property("St. James Place", 60, 30, false);
 	boardAry[17] = CommChest;
-	boardAry[18] = new Property("Mediterranean Avenue", 60, 30, false);
-	boardAry[19] = new Property("Mediterranean Avenue", 60, 30, false);
+	boardAry[18] = new Property("Tennessee Avenue", 60, 30, false);
+	boardAry[19] = new Property("New York Avenue", 60, 30, false);
 	boardAry[20] = FreePkg;
-	boardAry[21] = new Property("Mediterranean Avenue", 60, 30, false);
+	boardAry[21] = new Property("Kentucky Avenue", 60, 30, false);
 	boardAry[22] = Chance;
-	boardAry[23] = new Property("Mediterranean Avenue", 60, 30, false);
-	boardAry[24] = new Property("Mediterranean Avenue", 60, 30, false);
-	boardAry[25] = new RailRoad("Pennsylvania Railroad", 200, 60, false, 1);
-	boardAry[26] = new Property("Mediterranean Avenue", 60, 30, false);
-	boardAry[27] = new Property("Mediterranean Avenue", 60, 30, false);
+	boardAry[23] = new Property("Indiana Avenue", 60, 30, false);
+	boardAry[24] = new Property("Illinois Avenue", 60, 30, false);
+	boardAry[25] = new RailRoad("B&O Railroad", 200, 60, false, 1);
+	boardAry[26] = new Property("Atlantic Avenue", 60, 30, false);
+	boardAry[27] = new Property("Ventor Avenue", 60, 30, false);
 	boardAry[28] = Utility;
-	boardAry[29] = new Property("Mediterranean Avenue", 60, 30, false);
+	boardAry[29] = new Property("Marvin Gardens", 60, 30, false);
 	boardAry[30] = GoToJail;
-	boardAry[31] = new Property("Mediterranean Avenue", 60, 30, false);
-	boardAry[32] = new Property("Mediterranean Avenue", 60, 30, false);
+	boardAry[31] = new Property("Pacific Avenue", 60, 30, false);
+	boardAry[32] = new Property("North Carolina Avenue", 60, 30, false);
 	boardAry[33] = CommChest;
-	boardAry[34] = new Property("Mediterranean Avenue", 60, 30, false);
-	boardAry[35] = new RailRoad("Pennsylvania Railroad", 200, 60, false, 1);
+	boardAry[34] = new Property("Pennsylvania Avenue", 60, 30, false);
+	boardAry[35] = new RailRoad("Short Line", 200, 60, false, 1);
 	boardAry[36] = Chance;
-	boardAry[37] = new Property("Mediterranean Avenue", 60, 30, false);
+	boardAry[37] = new Property("Park Place", 60, 30, false);
 	boardAry[38] = LuxTax;
-	boardAry[39] = new Property("Mediterranean Avenue", 60, 30, false);
+	boardAry[39] = new Property("Boardwalk", 60, 30, false);
 	
-	//Write a function that takes in the current player's location, then prompts
-	//Based on the location for that particular property...
 	
-	function buy(location){
-		//First check if location is a purchaseable property
-		if("owned" in boardAry[location]){
-			document.getElementById("messageDisplay").innerHTML = "<h5>"+"This property is available"+"</h5>";
-			var btn = document.createElement("BUTTON");
-			var text = document.createTextNode("Buy Property");
-			btn.appendChild(text); //Putting text into button
-			btn.setAttribute("id", "buyBtn");
-			
-			document.getElementById("messageDisplay").appendChild(btn);
-			document.getElementById("buyBtn").onclick = function handleBuy(){
-			var buyPrmt = confirm("Are you sure you want to buy this property?");
-				}
-			}//End if
-			else{
-			document.getElementById("messageDisplay").innerHTML = "<h5>"+"Not purchasable"+"</h5>";
-			}	
-	}//End function Buy
 	
 
 	
